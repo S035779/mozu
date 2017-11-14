dcb95f12a98950aa199efa4b5b0f0e274fd29ed8
Mamoru Hashimoto <mamoru_hashimoto@hotmail.com>
2017-11-14 22:47:57 +0900

    modified src/server.js

---
+++ utils/apputils.js
@@ -0,0 +1,123 @@
+require('dotenv').config();
+import std from './stdutils';
+import htp from './netutils';
+import {logs as log} from './logutils';
+
+//Yahoo! OpenID API
+const yconnect_v2 = process.env.auth;
+
+/**
+ * get result of the 'Yahoo! Auth Endpoints & support types.'.
+ *
+ * @param callback {function}
+ */
+const YHAuthSupport = (callback) => {
+  const uri = yconnect_v2 + '.well-known/openid-configuration';
+  htp.get2(uri, (status, header, body) => {
+    const head = { request: uri, status, header };
+    const res = JSON.parse(body);
+    const obj = std.merge(head, { body: res });
+    if(head.status !== 200) {
+      YHerrorAuth(obj);
+      callback(new Error('Response Error!!'));
+    }
+    const authApi = obj.body.authorization_endpoint;
+    const toknApi = obj.body.token_endpoint;
+    const userApi = obj.body.userinfo_endpoint;
+    const jwksApi = obj.body.jwks_uri;
+    const support = obj.body.response_types_supported;
+    callback(null, {authApi, toknApi, userApi, jwksApi, support});
+  });
+};
+module.exports.YHAuthSupport = YHAuthSupport;
+
+/**
+ * get result of the 'Yahoo! JWKs.'.
+ *
+ * @param callback {function}
+ */
+const YHJwks = (callback) => {
+  const uri = yconnect_v2 + 'jwks';
+  htp.get2(uri, (status, header, body) => {
+    const head = { request: uri, status, header };
+    const res = JSON.parse(body);
+    const obj = std.merge(head, { body: res });
+    if(head.status !== 200) {
+      YHerrorAuth(obj);
+      callback(new Error('Response Error!!'));
+    }
+    const set = obj.body.keys;
+    const kids = set.map(o => ({
+      keyid:       o.kid
+      , modulus:   o.n
+      , exponent:  o.e
+    }));
+    callback(null, kids);
+  });
+};
+module.exports.YHJwks = YHJwks;
+
+/**
+ * get result of the 'Yahoo! Public Keys.'.
+ *
+ * @param callback {function}
+ */
+const YHPublicKeys = (callback) => {
+  const uri = yconnect_v2 + 'public-keys';
+  htp.get2(uri, (status, header, body) => {
+    const head = { request: uri, status, header };
+    const res = JSON.parse(body);
+    const obj = std.merge(head, { body: res });
+    if(head.status !== 200) {
+      YHerrorAuth(obj);
+      callback(new Error('Response Error!!'));
+    }
+    const set = obj.body;
+    callback(null, set);
+  });
+};
+module.exports.YHPublicKeys = YHPublicKeys;
+
+/**
+ * get result of the 'Yahoo! Authorization.'.
+ *
+ * @param options {object}
+ * @param callback {function}
+ */
+const YHAccessToken = (options, callback) => {
+  const uri = yconnect_v2 + 'token'
+     + '?' + std.encodeFormData(options);
+  htp.post2(uri, {}, {}, (status, header, body) => {
+    const head = { request: uri, status, header };
+    const res = JSON.parse(body);
+    const obj = std.merge(head, { body: res });
+    log.trace(obj);
+    if(head.status !== 200) {
+      YHerrorAuth(obj);
+      callback(new Error('Response Error!!'));
+    }
+    const set = obj.body;
+    callback(null, set);
+  });
+};
+module.exports.YHAccessToken = YHAccessToken;
+
+const YHerrorAuth = obj => {
+  let code = new Object();
+  code['nteraction_required'] = 'The Authorization Server requires End-User interaction of some form to proceed. This error MAY be returned when the prompt parameter value in the Authentication Request is none, but the Authentication Request cannot be completed without displaying a user interface for End-User interaction.';
+  code['login_required'] = 'The Authorization Server requires End-User authentication. This error MAY be returned when the prompt parameter value in the Authentication Request is none, but the Authentication Request cannot be completed without displaying a user interface for End-User authentication.';
+  code['account_selection_required'] = 'The End-User is REQUIRED to select a session at the Authorization Server. The End-User MAY be authenticated at the Authorization Server with different associated accounts, but the End-User did not select a session. This error MAY be returned when the prompt parameter value in the Authentication Request is none, but the Authentication Request cannot be completed without displaying a user interface to prompt for a session to use.';
+  code['consent_required'] = 'The Authorization Server requires End-User consent. This error MAY be returned when the prompt parameter value in the Authentication Request is none, but the Authentication Request cannot be completed without displaying a user interface for End-User consent.'; 
+  code['invalid_request_uri'] = 'The request_uri in the Authorization Request returns an error or contains invalid data.';
+  code['invalid_request_object'] = 'The request parameter contains an invalid Request Object.';
+  code['request_not_supported'] = 'The OP does not support use of the request parameter defined in Section 6.';
+  code['request_uri_not_supported'] = 'The OP does not support use of the request_uri parameter defined in Section 6.';
+  code['registration_not_supported'] = 'The OP does not support use of the registration parameter defined in Section 7.2.1.';
+  return `Error code: ${obj.error}` 
+    + `, response: ${code[obj.error]}`
+    + `, description: ${obj.error_description}`
+    + `, uri: ${obj.error_uri}`
+    + `, status: ${obj.state}`
+  ;
+};
+
--- utils/dbsutils.js
+++ utils/dbsutils.js
@@ -1,7 +1,118 @@
+require('dotenv').config();
 import std from './stdutils';
+import app from './apputils';
+import {logs as log} from './logutils';
+
+const AppUrl = process.env.app_url;
+const AppID = process.env.app_id;
+const Secret = process.env.secret;
+
+log.config('console', 'color', 'note-app', 'ALL');
 
 let tokens = [];
-const appid = 'watch_note!'
+
+/**
+ * 
+ *
+ * @returns {undefined}
+ */
+const fetchAuthSupport = (client, callback) => {
+  if(client.id !== AppID) callback({error: 'Unknown AppID!!'});
+  app.YHAuthSupport((err, set) => {
+    if(err) callback(err);
+    callback(set);
+  });
+};
+module.exports.fetchAuthSupport = fetchAuthSupport;
+
+/**
+ * 
+ *
+ * @returns {undefined}
+ */
+const fetchAuthJwks = (client, callback) => {
+  if(client.id !== AppID) callback({error: 'Unknown AppID!!'}); 
+  app.YHJwks((err, set) => {
+    if(err) callback(err);
+    callback(set);
+  });
+};
+module.exports.fetchAuthJwks = fetchAuthJwks;
+
+/**
+ * 
+ *
+ * @returns {undefined}
+ */
+const fetchAuthPublicKeys = (client, callback) => {
+  if(client.id !== AppID) callback({error: 'Unknown AppID!!'}); 
+  app.YHPublicKeys((err, set) => {
+    if(err) callback(err);
+    callback(set[client.keyid]);
+  });
+};
+module.exports.fetchAuthPublicKeys = fetchAuthPublicKeys;
+
+/**
+ * 
+ *
+ * @returns {undefined}
+ */
+const createAuthToken = (client, callback) => {
+  if(client.id !== AppID) callback({error: 'Unknown AppID!!'}); 
+  let options = new Object();
+  options['grant_type'] = 'authorization_code';
+  options['client_id'] = AppID;
+  options['client_secret'] = Secret;
+  options['redirect_uri'] = AppUrl;
+  options['code'] = client.code;
+  app.YHAccessToken(options, (err, set) => {
+    if (err) callback(err);
+    log.trace('Result:', set);
+    const newToken = {
+      code:             client.code
+      , access_token:   set.access_token
+      , refresh_token:  set.refresh_token
+      , expires_in:     set.expires_in
+      , id_token:       set.id_token
+    };
+    const isSuccess = hasToken(client)
+      ? false : addToken(newToken);
+    if(!isSuccess) callback({error: 'Create Token Error!!'});
+    callback(newToken);
+  });
+};
+module.exports.createAuthToken = createAuthToken;
+
+/**
+ * 
+ *
+ * @returns {undefined}
+ */
+const refreshAuthToken = (client, callback) => {
+  if(client.id !== AppID) callback({error: 'Unknown AppID!!'}); 
+  let options = new Object();
+  options['grant_type'] = 'refresh_token';
+  options['client_id'] = AppID;
+  options['client_secret'] = Secret;
+  options['refresh_token'] = client.code;
+  app.YHAccessToken(options, (err, set) => {
+    if (err) callback(err);
+    log.trace('Result:', set);
+    const newToken = {
+      code:             client.code
+      , access_token:   set.access_token
+      , refresh_token:  set.refresh_token
+      , expires_in:     set.expires_in
+      , id_token:       set.id_token
+    };
+    const isSuccess = hasToken(client)
+      ? updToken(client, newToken) : false;
+    if(!isSuccess) callback({error: 'Update Token Error!!'});
+    callback(newToken);
+  });
+};
+module.exports.refreshAuthToken = refreshAuthToken;
 
 /**
  * 
@@ -9,14 +120,9 @@ const appid = 'watch_note!'
  * @returns {undefined}
  */
 const deleteToken = (client, callback) => {
-  if(client.id !== appid) {
-    callback({error: 'Unknown AppID!!'}); 
-    return;
-  }
+  if(client.id !== AppID) callback({error: 'Unknown AppID!!'}); 
   const isSuccess = delToken(client)
-  if(!isSuccess) {
-    callback({error: 'Delete Token Error!!'});
-  }
+  if(!isSuccess) callback({error: 'Delete Token Error!!'});
   callback();
 };
 module.exports.deleteToken = deleteToken;
@@ -27,14 +133,9 @@ module.exports.deleteToken = deleteToken;
  * @returns {undefined}
  */
 const fetchToken = (client, callback) => {
-  if(client.id !== appid) {
-    callback({error: 'Unknown AppID!!'}); 
-    return;
-  }
+  if(client.id !== AppID) callback({error: 'Unknown AppID!!'}); 
   const token = hasToken(client);
-  if(!token) {
-    callback({error: 'Token not found!!'});
-  }
+  if(!token) callback({error: 'Token not found!!'});
   callback(null, token)
 };
 module.exports.fetchToken = fetchToken;
@@ -45,61 +146,10 @@ module.exports.fetchToken = fetchToken;
  * @returns {undefined}
  */
 const fetchTokens = (client, callback) => {
-  if(client.id !== appid) {
-    callback({error: 'Unknown AppID!!'}); 
-    return;
-  }
+  if(client.id !== AppID) callback({error: 'Unknown AppID!!'}); 
   callback(null, allToken())
 };
 module.exports.fetchTokens = fetchTokens;
-
-/**
- * 
- *
- * @returns {undefined}
- */
-const createToken = (client, callback) => {
-  if(client.id !== appid) {
-    callback({error: 'Unknown AppID!!'}); 
-    return;
-  }
-  const newToken = {
-    code:       std.makeRandStr(4)
-    , token:    std.makeRandStr(8)
-    , id_token: std.makeRandStr(16)
-  };
-  const isSuccess = hasToken(client)
-    ? false : addToken(newToken);
-  if(!isSuccess) {
-    callback({error: 'Create Token Error!!'});
-  }
-  callback();
-};
-module.exports.createToken = createToken;
-
-/**
- * 
- *
- * @returns {undefined}
- */
-const refreshToken = (client, callback) => {
-  if(client.id !== appid) {
-    callback({error: 'Unknown AppID!!'}); 
-    return;
-  }
-  const newToken = {
-    code:       client.code
-    , token:    std.makeRandStr(8)
-    , id_token: std.makeRandStr(16)
-  };
-  const isSuccess = hasToken(client)
-    ? updToken(client, newToken) : false;
-  if(!isSuccess) {
-    callback({error: 'Update Token Error!!'});
-  }
-  callback();
-};
-module.exports.refreshToken = refreshToken;
 
 /**
  * 
